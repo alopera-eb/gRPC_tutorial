0560a34879d7a5833595120e2215d741147bbab3
Ana Lopera <alopera@eventbrite.com>
2021-12-14 13:22:14 +0100

    Updating proto

---
+++ overcooked/data.py
@@ -0,0 +1,48 @@
+RECIPES_DATA = {
+    "cake": 
+        {
+            "imageUrl": "https://static.wikia.nocookie.net/overcooked/images/5/55/O2_Recipe_Cake.jpg/revision/latest?cb=20180830030705",
+            "ingredients": ["flour", "egg", "honey", "chocolate"],
+            "difficulty": 4,
+            "price": 20
+        },
+    "burger": 
+        {
+            "imageUrl": "https://static.wikia.nocookie.net/overcooked/images/9/94/O2_Recipe_Burger.jpg/revision/latest/scale-to-width-down/883?cb=20180828203547",
+            "ingredients": ["meat", "letucce", "tomato", "cheese", "bread"],
+            "difficulty": 3,
+            "price": 12
+        },
+    "burrito":
+        {
+            "imageUrl": "https://static.wikia.nocookie.net/overcooked/images/a/a2/BurritoRecipe.png/revision/latest/scale-to-width-down/1000?cb=20190624141730",
+            "ingredients": ["tortilla", "rice", "chicken"],
+            "difficulty": 3,
+            "price": 10
+        },
+    "sushi":
+        {
+            "imageUrl": "",
+            "ingredients": ["fish", "rice", "chicken"]
+            "difficulty": 3,
+            "price": 16
+        },
+    "salad":
+        {
+            "imageUrl": "",
+            "ingredients": ["lettuce", "tomato"]
+            "difficulty": 1,
+            "price": 8
+        },
+    "sashimi":
+        {
+            "imageUrl": "https://static.wikia.nocookie.net/overcooked/images/8/8c/O2_Recipe_Sashimi.jpg/revision/latest?cb=20180828203830",
+            "ingredients": ["fish"]
+            "difficulty": 1,
+            "price": 14
+        },
+}
+
+
+AVAILABLE_INGREDIENTS = [ "flour", "egg", "honey", "chocolate", "meat", "lettuce", 
+    "tomato", "cheese", "bread", "tortilla", "rice", "chicken", "fish", "nori" ]
